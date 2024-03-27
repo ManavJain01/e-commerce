9b7dd4c6a6190ddf20b5da2c9304e37be06c9ca7
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-03-27 22:39:13 +0530

    commit

--- src/Start/Major Project/Dismefa/Layout.jsx
+++ src/Start/Major Project/Dismefa/Layout.jsx
@@ -1,5 +1,6 @@
 import Header from './Main Page/components/Header/Header'
 import Footer from './Main Page/components/Footer/Footer'
+import ScrollToTop from './Main Page/components/ScrollToTop'
 
 import { Outlet } from 'react-router-dom'
 import { createGlobalStyle } from "styled-components"
@@ -17,6 +18,7 @@ function Layout(){
   return(
     <>
       <div className='overflow-x-hidden'>
+        <ScrollToTop />
         <Header />
         <Outlet />
         <Footer />
