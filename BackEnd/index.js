f74aaf94c8056832eb364b7bce4b0d43c532a2ba
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-09 12:47:25 +0530

    commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,25 +1,54 @@
+// Accessing Express and MongoDB Packages
 const express = require('express')
 const mongoose = require('mongoose')
-
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/dismefa')
+// Connecting MongoDB DataBase
+mongoose.connect('mongodb://localhost:27017/practice')
+.then(()=>console.log("MongoDB Connected."))
+.catch(err => console.log("Mongo Error" + err))
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  age: Number
+// Creating Schema and Model
+const ItemSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  jobTitle: {
+    type: String
+  },
+  gender: {
+    type: String
+  }
 })
 
-const UserModel = mongoose.model("dismefa", UserSchema)
+const ItemModel = mongoose.model("users", ItemSchema)
 
-app.get("/getUsers", (req, res)=>{
-  UserModel.find({}).then(function(users){
+
+// Routes
+app.get("/users", (req, res)=>{
+  ItemModel.find({}).then(function(users){
     res.json(users)
   }).catch(function(err){
     console.log(err);
   })
 })
 
+
+
+
+
+
+
+// Starting the server
 app.listen(5000, ()=>{
   console.log("Server is running on port 5000.");
 })
